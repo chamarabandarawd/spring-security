2551bfcb737ea12a2f887bbd24d69e24875430e5
chamarabandarawd <chamarabandarawd@gmail.com>
2023-10-05 13:05:08 +0530

    ExtractAllclaim methode done

--- src/main/java/com/example/jwtdemo/config/JwtService.java
+++ src/main/java/com/example/jwtdemo/config/JwtService.java
@@ -1,11 +1,36 @@
 package com.example.jwtdemo.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY="b7b54a2f63236120aa2844fa7c849600f05393a613ddcaa2b8bd4882758e22eb";
+
     public String extractUsername(String token){
         return null;
     }
+
+    private Claims extarctAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes= Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
 
 }
